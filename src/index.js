85dfa0f71e49b3f31f087b2fcb1eda01fdc1bf93
5n0weirdo <snoweirdo@protonmail.com>
2021-10-15 06:06:13 +0200

    Created a title component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-
 function Booklist() {
   return (
-    <Book />
+    <section>
+      <Book />
+    </section>
   );
 }
 
 const Book = () => {
-  return <article>
-
-
-  </article>
+  return (
+    <article>
+      <Image />
+      <Title />
+    </article>
+  );
 };
 
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
+      alt=""
+    />
+  );
+};
 
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>
+}
+
+// const Author = () => {
+//   return <h4>
+//     Amelia Hepworth
+//   </h4>
+// }
 ReactDom.render(<Booklist />, document.getElementById("root"));
