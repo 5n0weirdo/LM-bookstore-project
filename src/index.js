3ee0d545c2bf5eb66d246e5ff0b02faecafdd8d5
5n0weirdo <snoweirdo@protonmail.com>
2021-10-15 05:46:55 +0200

    Created a book component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import React from "react";
+import ReactDom from "react-dom";
 
+
+function Booklist() {
+  return (
+    <Book />
+  );
+}
+
+const Book = () => {
+  return <article>
+
+
+  </article>
+};
+
+
+ReactDom.render(<Booklist />, document.getElementById("root"));
