42c9f19e6ea93f51f583a4daf5c30e8705a5d22f
5n0weirdo <snoweirdo@protonmail.com>
2021-10-15 07:21:54 +0200

    Added JS

--- src/index.js
+++ src/index.js
@@ -7,14 +7,6 @@ import "./index.css";
 function Booklist() {
   return (
     <section className="booklist">
-      {/* <Heading /> */}
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
       <Book />
       <Book />
@@ -22,38 +14,20 @@ function Booklist() {
     </section>
   );
 }
-// const Heading = () => {
-//   return <h1>Booklist:</h1>;
-// };
 
 const Book = () => {
+  const title = "I Love You to the Moon and Back";
+  const author = "Amelia Hepworth";
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
-  );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>I Love You to the Moon and Back</h1>;
-};
-
-const Author = () => {
-  return (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", margin: "0.25rem" }}>
-      Amelia Hepworth
-    </h4>
   );
 };
 
