545518b61f3615b25692ecb1ca7fefe423843cf4
5n0weirdo <snoweirdo@protonmail.com>
2021-10-16 10:39:12 +0200

    Imported a file

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+    {
+        id: 1,
+        image:
+            "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
+        title: "I Love You to the Moon and Back",
+        author: "Amelia Hepworth",
+    },
+    {
+        id: 2,
+        image: "https://m.media-amazon.com/images/I/41cF-Khdv+L.jpg",
+        title: "Apples Never Fall",
+        author: "Liane Moriarty",
+    },
+];
--- src/index.js
+++ src/index.js
@@ -3,21 +3,8 @@ import ReactDom from "react-dom";
 
 //CSS
 import "./index.css";
-const books = [
-  {
-    id: 1,
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
-    title: "I Love You to the Moon and Back",
-    author: "Amelia Hepworth",
-  },
-  {
-    id: 2,
-    image: "https://m.media-amazon.com/images/I/41cF-Khdv+L.jpg",
-    title: "Apples Never Fall",
-    author: "Liane Moriarty",
-  },
-];
+
+import { books } from "./books";
 
 function Booklist() {
   return (
