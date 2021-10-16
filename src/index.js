745ccab99ac94311d7fe865911df14c93b770211
5n0weirdo <snoweirdo@protonmail.com>
2021-10-16 08:41:38 +0200

    Map over books

--- src/index.js
+++ src/index.js
@@ -3,52 +3,47 @@ import ReactDom from "react-dom";
 
 //CSS
 import "./index.css";
-const firstBook = {
-  image:
-    "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth",
-};
-
-const secondBook = {
-  image: "https://m.media-amazon.com/images/I/41cF-Khdv+L.jpg",
-  title: "Apples Never Fall",
-  author: "Liane Moriarty",
-};
+const books = [
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/41cF-Khdv+L.jpg",
+    title: "Apples Never Fall",
+    author: "Liane Moriarty",
+  },
+];
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book
-        image={firstBook.image}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
-          libero iste illum consectetur eveniet ut?
-        </p>
-        <button>Buy Now!</button>
-      </Book>
-      <Book
-        image={secondBook.image}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { image, title, author } = book;
+        return (
+          <div>
+            <img src={image} alt="" />
+            <h3>{title}</h3>
+            <h6>{author}</h6>
+          </div>
+        );
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
   //destructure:
-  const { image, title, author, children } = props;
+  const { image, title, author } = props;
+
   console.log(props);
   return (
     <article className="book">
       <img src={image} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
