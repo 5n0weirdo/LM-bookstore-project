e5417c4dad8915d77b80abc0042f085e349d36e6
5n0weirdo <snoweirdo@protonmail.com>
2021-10-16 10:49:01 +0200

    Imported and Exported

---
+++ src/Book.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+const Book = (props) => {
+    //destructure:
+    const { image, title, author } = props;
+
+    //attribute,eventHandler
+    //onClick, onMouseOver
+    const clickHandler = () => {
+        alert("Order");
+    };
+
+    const complexExample = (author) => {
+        console.log(author);
+    };
+
+    const imageHover = () => {
+        alert(title);
+    };
+
+    return (
+        <article className="book">
+            <img src={image} onMouseOver={imageHover} alt="" />
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+            <button onClick={clickHandler}>Order Now</button>
+            <button onClick={() => complexExample(author)}>
+                More complex example
+            </button>
+        </article>
+    );
+};
+
+
+export default Book
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "./index.css";
 
 import { books } from "./books";
 
+import Book from "./Book";
+
 function Booklist() {
   return (
     <section className="booklist">
@@ -15,36 +17,5 @@ function Booklist() {
     </section>
   );
 }
-
-const Book = (props) => {
-  //destructure:
-  const { image, title, author } = props;
-
-  //attribute,eventHandler
-  //onClick, onMouseOver
-  const clickHandler = () => {
-    alert("Order");
-  };
-
-  const complexExample = (author) => {
-    console.log(author);
-  };
-
-  const imageHover = () => {
-    alert(title);
-  };
-
-  return (
-    <article className="book">
-      <img src={image} onMouseOver={imageHover} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <button onClick={clickHandler}>Order Now</button>
-      <button onClick={() => complexExample(author)}>
-        More complex example
-      </button>
-    </article>
-  );
-};
 
 ReactDom.render(<Booklist />, document.getElementById("root"));
