aa24cb12d4aaf4e2293607c8e533333b7bf33e99
5n0weirdo <snoweirdo@protonmail.com>
2021-10-16 05:56:31 +0200

    Created books with props

--- src/index.js
+++ src/index.js
@@ -3,30 +3,42 @@ import ReactDom from "react-dom";
 
 //CSS
 import "./index.css";
+const firstBook = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  image: "https://m.media-amazon.com/images/I/41cF-Khdv+L.jpg",
+  title: "Apples Never Fall",
+  author: "Liane Moriarty",
+};
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        image={firstBook.image}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        image={secondBook.image}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "I Love You to the Moon and Back";
-  const author = "Amelia Hepworth";
-
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
-        alt=""
-      />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.image} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
