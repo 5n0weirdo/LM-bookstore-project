3c520b357b95f2ef5cd51e613e87b21eb10ea634
5n0weirdo <snoweirdo@protonmail.com>
2021-10-16 09:00:27 +0200

    Use spread operator when mapping

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import ReactDom from "react-dom";
 import "./index.css";
 const books = [
   {
+    id: 1,
     image:
       "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
     title: "I Love You to the Moon and Back",
     author: "Amelia Hepworth",
   },
   {
+    id: 2,
     image: "https://m.media-amazon.com/images/I/41cF-Khdv+L.jpg",
     title: "Apples Never Fall",
     author: "Liane Moriarty",
@@ -21,14 +23,7 @@ function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { image, title, author } = book;
-        return (
-          <div>
-            <img src={image} alt="" />
-            <h3>{title}</h3>
-            <h6>{author}</h6>
-          </div>
-        );
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
@@ -38,7 +33,6 @@ const Book = (props) => {
   //destructure:
   const { image, title, author } = props;
 
-  console.log(props);
   return (
     <article className="book">
       <img src={image} alt="" />
